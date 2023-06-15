c6e7e1b2d454bc968e1cdf908b1fdefa4ba9bea1
Abdi Merera <abdimerera@gmail.com>
2023-06-15 09:00:19 -0700

    task5update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * Description: Prints the string "With proper grammar, but the outcome is a piece of art,"
- *              followed by a new line using the printf function.
- * Return: 0 on success
+ * main - A programme that prints a line with printf function
+ * Return: 0 (The programme runs a success)
  */
 int main(void)
 {
