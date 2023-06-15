a352025aca679c02ef567c2da2b01f70d2c17fc5
Abdi Merera <abdimerera@gmail.com>
2023-06-15 07:05:40 -0700

    commit 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-gcc -o cisfun "$CFILE"
+#include <stdio.h>
+
+int main(void)
+{
+    char *message = "Programming is like building a multilingual puzzle";
+
+    puts(message);
+
+    return 0;
+}
