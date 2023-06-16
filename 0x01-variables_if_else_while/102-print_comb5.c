e1a48029261f25a4d35ee2777cc52ba9c0c92394
Abdi Merera <abdimerera@gmail.com>
2023-06-16 05:49:18 -0700

    advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,24 +14,24 @@ int main(void)
 
 	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = 0; num2 < 100; num2++)
-		{
-			putchar('0' + num1 / 10);
-			putchar('0' + num1 % 10);
+	for (num2 = num1; num2 < 100; num2++)
+	{
+		putchar('0' + num1 / 10);
+		putchar('0' + num1 % 10);
 
-			putchar(' ');
+	putchar(' ');
 
-			putchar('0' + num2 / 10);
-			putchar('0' + num2 % 10);
+	putchar('0' + num2 / 10);
+	putchar('0' + num2 % 10);
 
-			if (!(num1 == 99 && num2 == 99))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+	if (!(num1 == 99 && num2 == 99))
+	    	{
+		putchar(',');
+		putchar(' ');
 		}
 	}
+	}
 
-	putchar('\n');
-	return (0);
+    putchar('\n');
+    return (0);
 }
